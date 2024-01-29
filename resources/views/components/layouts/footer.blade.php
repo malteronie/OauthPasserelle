62eaf2fac5f31a7d76141bd693d59558072e48e0
Your Name <you@example.com>
2024-01-29 20:59:31 +0100

    contact online

--- resources/views/components/layouts/footer.blade.php
+++ resources/views/components/layouts/footer.blade.php
@@ -1,7 +1,11 @@
 <div class="w-full mt-6 h-28">
     <div class="text-center">
         @auth
-            <a href="{{ route('contact') }}">Contacter l'administrateur</a>
+            @if (env('APP_ONLINE'))
+                <a href="{{ route('contact') }}">Contacter l'administrateur</a>
+            @else
+                <a href=""></a>
+            @endif
         @endauth
         @guest
             <a href=""></a>
